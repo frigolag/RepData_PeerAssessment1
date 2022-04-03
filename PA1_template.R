aa9021efb1347b4199059c88f876c41279757ef9
Gerard <gfblazkez@hotmail.com>
2022-04-03 11:53:07 -0400

    2nd commit

--- PA1_template.R
+++ PA1_template.R
@@ -10,12 +10,32 @@ if (!file.exists(file)){
         unzip(zipfile)
 }
 
-## What is mean total number of steps taken per day?
-## Load packages
+## 1.Load the data
 library(tidyverse)
-mstep<-dataset%>%group_by(date)%>%summarise(mean=mean(steps),n=n())
-hist(mstep$mean)
-stpday<- with(dataset,tapply(steps, date, mean))
+df<-read_csv(file)
+## 2.Process data (remove NA's)
+df<-na.omit(df)
 
-hist<-ggplot(data=dataset)
-hist+aes(mean(steps))
+## What is mean total number of steps taken per day?
+## 1.Make a histogram of the total number of steps taken each day
+stepxday<-df%>%group_by(date)%>%summarise(steps=sum(steps))
+g_stepxday<- ggplot(stepxday, aes(date,steps)) + geom_col()
+print(g_stepxday)
+## 2.Calculate and report the mean and median total number of steps taken per day
+mean(stepxday$steps)
+median(stepxday$steps)
+
+## What is the average daily activity pattern?
+## 1.Make a time series plot (i.e. type = "l") of the 5-minute interval (x-axis) and the average number of steps taken, averaged across all days (y-axis)
+stepxint<-with(df,tapply(steps, interval, mean))
+stepxint<-as.tibble(as.data.frame(as.table(stepxint)))
+colnames(stepxint)<-c("interval","mean_steps")
+g_stepxint<-ggplot(stepxint,aes(x=interval,y=mean_steps,group=1))+geom_line()
+print(g_stepxint)
+
+## Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
+max_stp_int<-stepxint%>%arrange(desc(mean_steps))
+max_stp_int[1,]
+
+## Imputing missing values
+
