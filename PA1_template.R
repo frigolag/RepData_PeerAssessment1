3d5aa1eca2cec7c41688856eb68bb87fd2c965b9
Gerard <gfblazkez@hotmail.com>
2022-04-10 15:56:30 -0400

    Example how to create df from array (see df4,df5,df6)

--- PA1_template.R
+++ PA1_template.R
@@ -1,4 +1,7 @@
 ## Loading and preprocessing the data
+##Load packages
+library(tidyverse)
+library(gridExtra)
 ## Download data and unzip
 url<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
 zipfile<- "repdata_data_activity.zip"
@@ -11,17 +14,19 @@ if (!file.exists(file)){
 }
 
 ## 1.Load the data
-library(tidyverse)
 dataset<-read.csv(file)
 ## 2.Process data (remove NA's)
 df<-na.omit(dataset)
+df$date<-as.Date(df$date)
 
 ## What is mean total number of steps taken per day?
 ## 1.Make a histogram of the total number of steps taken each day
 stepxday<-df%>%group_by(date)%>%summarise(steps=sum(steps))
-g_stepxday<- ggplot(stepxday, aes(date,steps)) + geom_col()+
+g_stepxday<- ggplot(stepxday, aes(date,steps)) + 
+        geom_col(width = 0.5)+
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 print(g_stepxday)
+
 ## 2.Calculate and report the mean and median total number of steps taken per day
 mean(stepxday$steps)
 median(stepxday$steps)
@@ -31,16 +36,20 @@ median(stepxday$steps)
 stepxint<-with(df,tapply(steps, interval, mean))
 stepxint<-as.tibble(as.data.frame(as.table(stepxint)))
 colnames(stepxint)<-c("interval","mean_steps")
-g_stepxint<-ggplot(stepxint,aes(x=interval,y=mean_steps,group=1))+geom_line()
+stepxint$interval<-as.numeric(stepxint$interval)
+g_stepxint<-ggplot(stepxint,aes(interval,mean_steps))+geom_line()
 print(g_stepxint)
 
-## Which 5-minute interval, on average across all the days in the dataset, contains the maximum number of steps?
+## Which 5-minute interval, on average across all the days in the dataset, 
+##contains the maximum number of steps?
 max_stp_int<-stepxint%>%arrange(desc(mean_steps))
 max_stp_int[1,]
 
 ## Imputing missing values
-##1.Calculate and report the total number of missing values in the dataset (i.e. the total number of rows with NAs
+##1.Calculate and report the total number of missing values in the dataset 
+##(i.e. the total number of rows with NAs
 sum(is.na(dataset))
+
 ##2.Filling in all of the missing values in the dataset: the mean of that 5-minute interval
 narows<-is.na(dataset$steps)
 df2<-dataset
@@ -49,15 +58,49 @@ for (i in which(narows==TRUE)){
         mean_steps<-stepxint[stepxint$interval==int,2]
         df2[i,1]<-mean_steps
 }
+
 ## 4.Make a histogram of the total number of steps taken each day
 stepxday2<-df2%>%group_by(date)%>%summarise(steps=sum(steps))
-g_stepxday2<- ggplot(stepxday2, aes(date,steps)) + geom_col()+
+g_stepxday2<- ggplot(stepxday2, aes(date,steps)) + 
+        geom_col(width = 0.5)+
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 print(g_stepxday2)
+
 ## 4.Calculate and report the mean and median total number of steps taken per day
 mean(stepxday2$steps)
 median(stepxday2$steps)
 
-##Are there differences in activity patterns between weekdays and weekends?
-library(gridExtra)
 grid.arrange(g_stepxday,g_stepxday2,ncol=2)
+
+##Are there differences in activity patterns between weekdays and weekends?
+##1.Create a new factor variable in the dataset with two levels -- "weekday" and "weekend"
+days<- c(character())
+##df2[,2]<-as.Date(df2[,2])
+days<-weekdays(df2[,2])
+for (i in 1:length(days)){
+        if (days[i]=="Saturday"|days[i]=="Sunday"){
+                days[i]<-"weekend"
+        }
+        else {
+                days[i]<-"weekday"
+        } 
+}
+days<-as.factor(days)
+df3<-cbind(df2,days)
+
+##2.Make a panel plot containing a time series plot
+ind<-with(df3,list(interval,days))
+stepxint4<-with(df3,tapply(steps, ind, mean))
+## 1st option
+df4<-data.frame(interval=dimnames(stepxint4)[[1]],steps=stepxint4[,1],days="weekdays")
+df5<-data.frame(interval=dimnames(stepxint4)[[1]],steps=stepxint4[,2],days="weekend")
+df6<-rbind(df4,df5)
+##2nd option
+stepxint3<-as.tibble(as.data.frame(as.table(stepxint4)))
+colnames(stepxint3)<-c("interval","days","mean_steps")
+
+##Plot
+xyplot(mean_steps~interval|days,data=stepxint3,layout=c(1,2),
+       type="l",scales=list(x=list(rot=90,at=seq(0,300,by=25))),
+       ylab = "Number of steps")
+
